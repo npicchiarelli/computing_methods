23eb53fd7cf267886c374a2827b60fb550b7e5c8
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-03-15 08:18:00 +0100

    added pad_awkward_array

--- utils/utils.py
+++ utils/utils.py
@@ -122,7 +122,8 @@ def file_names_map(files: list, separator: str, enc_dict: dict):
     return df
 
 class DatasetError(Exception):
-    pass
+    """Class for error in
+    dataset selection"""
 
 def encode_file_names(files: list, encoding, **kwargs):
     """Extends file_names_map functionalities adding specifical features
@@ -155,7 +156,7 @@ def encode_file_names(files: list, encoding, **kwargs):
         if encoding == 'ravdess':
             if str(files[0].name)[2] == "-":
                 separator = "-"
-                CATEGORICAL_FEATURES_NAMES = {
+                categorical_features_names = {
                     "modality": {"01": "full-AV", "02": "video-only", "03": "audio-only"}, 
                     "vocal_channel": {"01": "speech", "02": "song"},
                     "emotion": {"01" : "neutral",
@@ -172,29 +173,70 @@ def encode_file_names(files: list, encoding, **kwargs):
                     "repetition": {"01" : "1st", "02" : "2nd"},
                     "actor": {str(i).zfill(2): str(i).zfill(2) for i in range(1, 25)}
                 }
-                df = file_names_map(files, separator, CATEGORICAL_FEATURES_NAMES)
+                df = file_names_map(files, separator, categorical_features_names)
                 df["sex"] = ["F" if i % 2 == 0 else "M" for i in df["actor"].astype(int)]
             else:
-                raise DatasetError(f'Wrong separator in file names: {str(files[0].name)[2]} was given but - was expected.')
+                raise DatasetError(f'Wrong separator in file names:'
+                                   f'{str(files[0].name)[2]} was given but - was expected.')
         elif encoding == 'crema':
             if str(files[0].name)[4] == "_":
                 separator = "_"
-                CATEGORICAL_FEATURES_NAMES = {
+                categorical_features_names = {
                     "actor": {f'10{str(i).zfill(2)}': str(i).zfill(2) for i in range(1, 92)},
-                    "statement": {'IEO': "It's eleven o'clock", 'TIE': "That is exactly what happened", 'IOM': "I'm on my way to the meeting", 'IWW': "I wonder what this is about", 'TAI': "The airplane is almost full", 'MTI': "Maybe tomorrow it will be cold", 'IWL': "I would like a new alarm clock", 'ITH': "I think I have a doctor's appointment", 'DFA': "Don't forget a jacket", 'ITS': "I think I've seen this before", 'TSI': "The surface is slick", 'WSI': "We'll stop in a couple of minutes"},
-                    "emotion": {'ANG': 'anger', 'DIS': 'disgust', 'FEA': 'fear', 'HAP': 'happy', 'NEU': 'neutral', 'SAD': 'sad'},
-                    "emotional_intensity": {'LO': 'Low', 'MD': 'Medium', 'HI': 'High', 'XX': 'Unspecified'},
+                    "statement": {'IEO': "It's eleven o'clock",
+                                  'TIE': "That is exactly what happened",
+                                  'IOM': "I'm on my way to the meeting",
+                                  'IWW': "I wonder what this is about",
+                                  'TAI': "The airplane is almost full",
+                                  'MTI': "Maybe tomorrow it will be cold",
+                                  'IWL': "I would like a new alarm clock",
+                                  'ITH': "I think I have a doctor's appointment",
+                                  'DFA': "Don't forget a jacket",
+                                  'ITS': "I think I've seen this before",
+                                  'TSI': "The surface is slick",
+                                  'WSI': "We'll stop in a couple of minutes"},
+                    "emotion": {'ANG': 'anger', 'DIS': 'disgust', 'FEA': 'fear',
+                                'HAP': 'happy', 'NEU': 'neutral', 'SAD': 'sad'},
+                    "emotional_intensity": {'LO': 'Low', 'MD': 'Medium',
+                                            'HI': 'High',
+                                            'XX': 'Unspecified'},
                 }
-                df = file_names_map(files, separator, CATEGORICAL_FEATURES_NAMES)
+                df = file_names_map(files, separator, categorical_features_names)
             else:
-                raise  DatasetError(f'Wrong separator in file names: {str(files[0].name)[4]} was given but - was expected.')
+                raise  DatasetError(f'Wrong separator in file names:'
+                                    f'{str(files[0].name)[4]} was given but - was expected.')
         else:
             raise DatasetError('No dataset encoding for the selected dataset name!')
-        
+
     elif isinstance(encoding, dict):
         df = file_names_map(files, kwargs["separator"], encoding)
-    
+
     else:
         raise TypeError('The input encoding must be of type str ("ravdess" or "crema") or dict.')
-    
+
     return df
+
+def pad_awkward_array(x: ak.Array, nan_value = 0.):
+    """Takes as input a 3D awkward array,
+    returns a 3D awkward.Array padded with nan_value
+    to have all the rows of the same length.
+
+    Arguments
+    ---------
+    x : ak.Array
+        The array to pad.   
+    nan_value : float
+        the value used to pad 
+
+    Return
+    ---------
+    The padded array : ak.Array
+    """
+
+    maximum = 0
+    for ts in x:
+        length = len(np.asarray(np.ravel(ts)))
+        if length > maximum:
+            maximum = length
+
+    return ak.fill_none(ak.pad_none(x, maximum, axis=2, clip=True), value=nan_value)
